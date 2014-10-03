a72c42ad8db41ba3d0460bff224e011e24632eda
ktbee <kbroida@gmail.com>
2014-10-02 21:57:58 -0400

    Finished the password logic. Justy need to pretty up the html

--- logic.php
+++ logic.php
@@ -16,20 +16,52 @@
 			'Dutch'
 		);
 
+	$symList = Array(
+			'!',
+			'@',
+			'#',
+			'$',
+			'%',
+			'^',
+			'&',
+			'*',
+			'~'
+		);
+
+
 	 
 	$numWords = $_POST['numWord'] - 1;
 	$newPassword = Array();
+	$numInclude = $_POST['numInclude'];
+	$symInclude = $_POST['symInclude'];
 
 	for($i=0; $i<=$numWords; $i++){
 		$wordPosition = array_rand($wordList);
 		$newWord = $wordList[$wordPosition];
 		array_push($newPassword, $newWord);
+		array_push($newPassword, "-");
 	}
 
+	//foreach( $newPassword as $word){
+	//	echo $word;
+	//}
 
-	for($i = 0, $size = count($newPassword); $i < $size; $i++){
-			echo $newPassword[$i].'-';
+	$symPosition = array_rand($symList);
+	$randSym = $symList[$symPosition];
+
+	for($i = 0, $size = count($newPassword) - 1; $i < $size; $i++){
+			echo $newPassword[$i];
 	}
+
+	if ($numInclude == 'on'){
+		$randNum = rand(0,9);
+		echo '-'.$randNum;
+	}
+
+	if ($symInclude == 'on'){
+		echo '-'.$randSym;
+	}
+
 
 
 
